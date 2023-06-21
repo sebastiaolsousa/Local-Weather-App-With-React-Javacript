6a7d59e4542a8d83c46ad0397065ac1e85777401
sebastiaolsousa <sebastiao.luzsousa@gmail.com>
2023-06-21 17:40:39 -0300

    almost there

--- local-weather-app/src/App.js
+++ local-weather-app/src/App.js
@@ -1,31 +1,80 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import axios from 'axios';
 import Weather from "./components/Weather";
 
 const App = () => {
+  const [cityName, setCityName] = useState('');
+  const [apiKey, setApiKey] = useState('');
   const [weatherData, setWeatherData] = useState(null);
+  const [showResult, setShowResult] = useState(false);
 
-  useEffect(() => {
-    const fetchData = async () => {
+ 
+  const fetchData = async () => {
+    try {
       const response = await axios.get(
-        'https://api.openweathermap.org/data/2.5/weather?lat=-48.50444&lon=-1.45583&appid=c06c15f02eff0811952e167b212c4286'
+        `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
       );
       setWeatherData(response.data);
-    };
-    fetchData();
-  }, []);
+      setShowResult(true);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const generateForecast = () => {
+    if (cityName && apiKey) {
+      fetchData();
+    } else {
+      window.alert('Please enter the city name and API key')
+    }
+  }
+
+  const resetForm = () => {
+    setCityName('');
+    setApiKey('');
+    setWeatherData(null);
+    setShowResult(false);
+  }
 
   return (
     <div className="local-weather-app">
       <h1>Local Weather App</h1>
-      {weatherData ? (
-        <Weather
-          city={weatherData.name}
-          temperature={weatherData.main.temp}
-          description={weatherData.weather[0].description}
-        />
+      {showResult ? (
+        <>
+          <Weather
+            city={weatherData.name}
+            weathericon={weatherData.icon}
+            temperature={weatherData.main.temp}
+            humidity={weatherData.main.humidity}
+            windSpeed={weatherData.wind.speed}
+            description={weatherData.weather[0].description}
+          />
+          <button onClick={resetForm}>Reset</button>
+        </>
       ) : (
-        <p>Loading...</p>
+        <>
+          <label htmlFor="cityNameInput">City Name: </label>
+          <input
+            type="text"
+            id="cityNameInput"
+            value={cityName}
+            placeholder="Ex: New York,US"
+            onChange={(e) => setCityName(e.target.value)}
+          />
+          <br/>
+          <label htmlFor="apiKeyInput">API Key: </label>
+          <input
+            type="text"
+            id="apiKeyInput"
+            value={apiKey}
+            placeholder="Register for free in the link bellow"
+            onChange={(e) => setApiKey(e.target.value)}
+          />
+          <br/>
+          <button onClick={generateForecast}>Generate Weather Forecast</button>
+          <br/>
+          <p>To create an API Key <a href="https://openweathermap.org/" target="blank">click here</a></p>
+        </>
       )}
     </div>
   );
--- local-weather-app/src/components/Weather.js
+++ local-weather-app/src/components/Weather.js
@@ -1,10 +1,24 @@
 import React from "react";
+import * as fa from 'react-icons/fa';
 
-const Weather = ({ city, temperature, description}) => {
+const Weather = ({ city, temperature, description, weatherType, humidity, windSpeed }) => {
+    let weatherIcon;
+
+    if (weatherType === 'cloudy') {
+        weatherIcon = <fa.FaCloud/>;
+    } else if (weatherType === 'sunny') {
+        weatherIcon = <fa.FaSun/>;
+    } else {
+        weatherIcon = <fa.FaCloudSun/>;
+    }
+    
     return (
         <div>
             <h2>{city}</h2>
-            <p>Temperature: {temperature}</p>
+            {weatherIcon}
+            <p>Temperature: {Math.floor(temperature-273)} ºC</p>
+            <p>Humidity: {humidity}%</p>
+            <p>Wind Speed: {windSpeed} Km/H</p>
             <p>Description: {description}</p>
         </div>
     );
